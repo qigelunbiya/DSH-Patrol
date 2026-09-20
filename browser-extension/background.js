d8273aec049efe14d77b64f1bd053a26bb7606d5
YueFang <128342898+qigelunbiya@users.noreply.github.com>
2026-09-20 09:11:08 +0800

    wip: advertise browser visual click capability

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -11,6 +11,7 @@ const EXTENSION_CAPABILITIES = Object.freeze([
   'directImageSource',
   'pageBridgeRetry',
   'semanticClick',
+  'visualClick',
 ])
 let socket = null
 let state = 'disconnected'
